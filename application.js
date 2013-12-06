ed395d4c67adae6f4cbb17e34efa2bad10fa698f
yoshinaria <yoshinariakiko@gmail.com>
2013-12-06 14:48:22 -0600

    final

--- application.js
+++ application.js
@@ -1,7 +1,17 @@
-$('document').ready(function() {
-    $('h1').css("background-color", "red");
-   $('.leftArrow').show();
-   });
-    
-
-});
+$(document).ready(function(){
+  $(".Basecamp").hover(function(){
+  		$(".defaultHeader").toggle(); 
+      	$(".BasecampHeader").toggle();  
+    	$("img.BaseArrow").toggle();
+  });
+  $(".Highrise").hover(function(){
+  		$(".defaultHeader").toggle(); 
+      	$(".HighriseHeader").toggle();  
+    	$("img.HighArrow").toggle();
+  });
+  $(".Campfire").hover(function(){
+  		$(".defaultHeader").toggle(); 
+      	$(".CampfireHeader").toggle();  
+    	$("img.CampArrow").toggle();
+  });
+ });
